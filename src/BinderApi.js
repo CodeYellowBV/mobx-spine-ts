b96595412cacdfa5e4b141b449b67cd4f6add323
stefanmajoor <stefan@codeyellow.nl>
2021-03-21 13:52:32 +0100

    First init of typescript + tests

---
+++ src/BinderApi.js
@@ -0,0 +1,9 @@
+"use strict";
+exports.__esModule = true;
+exports.BinderApi = void 0;
+var BinderApi = /** @class */ (function () {
+    function BinderApi() {
+    }
+    return BinderApi;
+}());
+exports.BinderApi = BinderApi;
---
+++ src/index.ts
@@ -0,0 +1,1 @@
+import { BinderApi} from "./BinderApi";
