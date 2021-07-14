d79ddb94d09e1a2251ceb1622534f27f2bcc5d73
knokko <knokogator@hotmail.com>
2021-07-14 16:04:46 +0200

    Add the Cast test suites

---
+++ src/__tests__/Casts/date.ts
@@ -0,0 +1,95 @@
+import { Model, Casts, ModelData, tsPatch } from '../../';
+import { observable } from 'mobx';
+import moment, { Moment } from 'moment';
+
+interface AnimalData extends ModelData {
+    birthDate: Moment;
+}
+
+@tsPatch
+class Animal extends Model<AnimalData> implements AnimalData {
+    @observable birthDate: Moment = null;
+
+    casts() {
+        return {
+            birthDate: Casts.date,
+        };
+    }
+}
+
+test('should parse to model', () => {
+    const animal = new Animal();
+
+    expect(animal.birthDate).toBe(null);
+
+    animal.parse({
+        birthDate: '1995-03-22',
+    });
+
+    expect(animal.birthDate).toBeInstanceOf(moment);
+    expect(animal.birthDate.format('YYYY-MM-DD')).toBe('1995-03-22');
+});
+
+test('parse should treat undefined as null', () => {
+    const animal = new Animal({ birthDate: '1995-03-22' });
+    expect(animal.birthDate).toBeInstanceOf(moment);
+
+    animal.parse({
+        birthDate: undefined,
+    });
+
+    expect(animal.birthDate).toBe(null);
+});
+
+test('toJS should treat undefined as null', () => {
+    const animal = new Animal({ birthDate: '1995-03-22' });
+    animal.birthDate = undefined;
+
+    expect(animal.toJS().birthDate).toBe(null);
+});
+
+test('should parse to model when null', () => {
+    const animal = new Animal({ birthDate: '1995-03-22' });
+    expect(animal.birthDate).toBeInstanceOf(moment);
+
+    animal.parse({
+        birthDate: null,
+    });
+
+    expect(animal.birthDate).toBe(null);
+});
+
+test('parse() should throw away time info', () => {
+    const animal = new Animal({ birthDate: '2017-03-22T22:08:23+00:00' });
+
+    expect(animal.birthDate.format('YYYY-MM-DD')).toBe('2017-03-22');
+});
+
+test('should be serialized in toJS()', () => {
+    const animal = new Animal({ birthDate: '1995-03-22' });
+
+    expect(animal.toJS()).toEqual({
+        birthDate: '1995-03-22',
+    });
+});
+
+test('should be serialized in toJS()', () => {
+    const animal = new Animal();
+
+    expect(animal.toJS()).toEqual({
+        birthDate: null,
+    });
+});
+
+test('toJS() should throw error when moment instance is gone', () => {
+    const animal = new Animal({ birthDate: '1995-03-22' });
+
+    // We need ts-ignore for this test because this isn't valid TypeScript, but it could still
+    // occur due to the crazy ways of JavaScript.
+    // @ts-ignore
+    animal.birthDate = 'asdf';
+
+    expect(() => {
+        return animal.toJS();
+    }).toThrow("[mobx-spine] Attribute 'birthDate' is not a moment instance.");
+});
---
+++ src/__tests__/Casts/datetime.ts
@@ -0,0 +1,130 @@
+import { Model, Casts, ModelData, tsPatch } from '../../';
+import { observable } from 'mobx';
+import { Settings, DateTime } from 'luxon';
+import moment, { Moment } from 'moment';
+import momentLocale from 'moment/min/moment-with-locales';
+
+interface AnimalData extends ModelData {
+    bornAt: Moment
+}
+
+@tsPatch
+class Animal extends Model<AnimalData> {
+    @observable bornAt: Moment = null;
+
+    casts() {
+        return {
+            bornAt: Casts.datetime,
+        };
+    }
+}
+
+// Unfortunately we can't check the whole datetime because the CI has a different timezone so that fucks things up.
+// I should really fix this but I really don't want to. Do you? Fuck timezones.
+
+test('should parse to model', () => {
+    const animal = new Animal();
+
+    expect(animal.bornAt).toBe(null);
+
+    animal.parse({
+        bornAt: '2017-03-22T22:08:23',
+    });
+
+    expect(animal.bornAt).toBeInstanceOf(moment);
+    expect(animal.bornAt.format()).toEqual(
+        expect.stringContaining('2017-03-22T')
+    );
+});
+
+test('should parse to model when null', () => {
+    const animal = new Animal({ bornAt: '2017-03-22T22:08:23+00:00' });
+    expect(animal.bornAt).toBeInstanceOf(moment);
+
+    animal.parse({
+        bornAt: null,
+    });
+
+    expect(animal.bornAt).toBe(null);
+});
+
+test('should be serialized in toJS()', () => {
+    const animal = new Animal({ bornAt: '2017-03-22T22:08:23+00:00' });
+
+    expect(animal.toJS().bornAt).toEqual(expect.stringContaining('2017-03-22'));
+});
+
+test('should be serialized in toJS() when null', () => {
+    const animal = new Animal();
+
+    expect(animal.toJS()).toEqual({
+        bornAt: null,
+    });
+});
+
+test('toJS() should throw error when moment instance is gone', () => {
+    const animal = new Animal({ bornAt: '2017-03-22T22:08:23+00:00' });
+
+    // @ts-ignore
+    animal.bornAt = 'asdf';
+
+    expect(() => {
+        return animal.toJS();
+    }).toThrow("[mobx-spine] Attribute 'bornAt' is not a moment instance.");
+});
+
+test('should be serialized in toBackend()', () => {
+    const animal = new Animal({ bornAt: '2017-03-22T22:08:23+00:00' });
+
+    expect(animal.toBackend()['born_at']).toEqual(
+        expect.stringContaining('2017-03-22')
+    );
+});
+
+test('moment instance with locale should be recognized', () => {
+    const animal = new Animal();
+    animal.bornAt = momentLocale('2017-03-22T22:08:23+00:00');
+    expect(animal.toJS().bornAt).toEqual(expect.stringContaining('2017-03-22'));
+});
+
+describe('luxon compatibility', () => {
+    Settings.defaultZoneName = 'utc';
+
+    interface LuxonAnimalData extends AnimalData {
+        createdAt?: DateTime
+    }
+
+    @tsPatch
+    class LuxonAnimal extends Animal {
+        @observable createdAt: DateTime = null;
+
+        casts() {
+            return {
+                bornAt: Casts.luxonDatetime,
+            };
+        }
+    };
+
+    test('toJS() should throw error when luxon instance is gone', () => {
+        const animal = new LuxonAnimal({ bornAt: '2017-03-22T22:08:23+00:00' });
+
+        // @ts-ignore
+        animal.bornAt = 'asdf';
+
+        expect(() => {
+            return animal.toJS();
+        }).toThrow("[mobx-spine] Attribute 'bornAt' is not a luxon instance.");
+    });
+
+    test('should be serialized in toBackend()', () => {
+        const animal = new LuxonAnimal({ bornAt: '2017-03-22T22:08:23+00:00' });
+
+        expect(animal.toBackend()['born_at']).toEqual('2017-03-22T22:08:23+00:00');
+    });
+
+    test('should be serialized in toBackend() when given a binder specific format', () => {
+        const animal = new LuxonAnimal({ bornAt: '2017-03-22T22:08:23.575242+0000' });
+
+        expect(animal.toBackend()['born_at']).toEqual('2017-03-22T22:08:23+00:00');
+    });
+});
