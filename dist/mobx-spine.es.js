d9f0c2639598b647fd533ab241374a2e1ade05c9
knokko <knokogator@hotmail.com>
2021-07-19 12:28:35 +0200

    Lets try again

---
+++ dist/mobx-spine.es.js
@@ -0,0 +1,24 @@
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Casts = exports.configureDateLib = exports.Store = exports.tsPatch = exports.Model = exports.BinderApi = void 0;
+var BinderApi_1 = require("./BinderApi");
+Object.defineProperty(exports, "BinderApi", { enumerable: true, get: function () {
+    return BinderApi_1.BinderApi;
+  } });
+var Model_1 = require("./Model");
+Object.defineProperty(exports, "Model", { enumerable: true, get: function () {
+    return Model_1.Model;
+  } });
+Object.defineProperty(exports, "tsPatch", { enumerable: true, get: function () {
+    return Model_1.tsPatch;
+  } });
+var Store_1 = require("./Store");
+Object.defineProperty(exports, "Store", { enumerable: true, get: function () {
+    return Store_1.Store;
+  } });
+var Casts_1 = require("./Casts");
+Object.defineProperty(exports, "configureDateLib", { enumerable: true, get: function () {
+    return Casts_1.configureDateLib;
+  } });
+Object.defineProperty(exports, "Casts", { enumerable: true, get: function () {
+    return Casts_1.Casts;
+  } });
